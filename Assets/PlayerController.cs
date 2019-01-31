db53e6e1f04150e5e36a5b57b0ccabc6009b13ae
Alexa Duarte <alexa-2095@hotmail.com>
2019-01-30 22:30:43 -0600

    Minor changes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -23,8 +23,18 @@ public class PlayerController : MonoBehaviour
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+        //transform.Translate(0, 0, translation);
+        //transform.Rotate(0, rotation, 0);
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(Vector3.left * translation);
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(Vector3.right *translation);
+        if (Input.GetKey(KeyCode.W))
+            rb.AddForce((new Vector3(0, 0, 1)) * translation);
+        if (Input.GetKey(KeyCode.S))
+            rb.AddForce((new Vector3(0, 0, -1)) * 10);
 
         if (translation != 0)
         {
