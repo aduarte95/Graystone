ede2834b9179130275b01c30f3be5c52c813a1b3
Alexa Duarte <alexa-2095@hotmail.com>
2019-01-30 20:51:40 -0600

    Forward and rotation's player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    static Animator animator;
+    int isWalkingHash = Animator.StringToHash("IsWalking");
+    public float speed = 2.0f;
+    public float rotationSpeed = 75.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation != 0)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+        else
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+    }
+}
--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -16,12 +16,12 @@ public class ControlPlayer : MonoBehaviour
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         if (Input.GetKey(KeyCode.A))
-            rb.AddForce(Vector3.left * 10);
+            rb.AddForce(Vector3.left * 100);
         if (Input.GetKey(KeyCode.D))
-            rb.AddForce(Vector3.right * 10);
+            rb.AddForce(Vector3.right * 100);
         if (Input.GetKey(KeyCode.W))
-            rb.AddForce((new Vector3(0,0,1)) * 10);
+            rb.AddForce((new Vector3(0,0,1)) * 100);
         if (Input.GetKey(KeyCode.S))
-            rb.AddForce((new Vector3(0, 0, -1)) * 10);
+            rb.AddForce((new Vector3(0, 0, -1)) * 100);
     }
 }
