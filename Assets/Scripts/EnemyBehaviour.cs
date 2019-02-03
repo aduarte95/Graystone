9301b8df0db4d8d39a0bd467240fb6325f34e8e3
Ross1301 <32336848+Ross1301@users.noreply.github.com>
2019-02-03 15:05:37 -0600

    enemy controller

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce((new Vector3(0, 0, 1)) * 100);
+    }
+
+    public void CollideWithHorizontal(Collider other)
+    {
+        direction = -direction;
+    }
+}
