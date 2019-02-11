fc271da572529c25a66aebc53711fb6d1cd28259
Ross1301 <32336848+Ross1301@users.noreply.github.com>
2019-02-11 09:45:37 -0600

    The alien does move git add -A

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -10,10 +10,16 @@ public class EnemyBehaviour : MonoBehaviour
     public float speed = 2.0f;
     public int direction = 1;
 
+
+    float random;
+    public float chanceOfCurrency = 0.005f;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,22 +27,49 @@ public class EnemyBehaviour : MonoBehaviour
     {
         float translation = direction * speed;
         translation *= Time.deltaTime;
-        Vector3 move = new Vector3(0, 0, translation); 
-        transform.Translate(move);
+        Vector3 move = new Vector3(0, 0, translation);
 
-        if (translation != 0)
+        random = Random.value;
+        if (random < chanceOfCurrency)
         {
-            animator.SetBool(isWalkingHash, true);
+            transform.Rotate(0, 90, 0);
         }
 
-        RaycastHit[] results = new RaycastHit[16];
+        RaycastHit hit;
 
-        //int cnt = GetComponent<Rigidbody>().Cast(move, results, move.magnitude + 0.01f); //This not work 
-        //if (cnt > 0)
-
-        if (results != null) //with this the alien does not move at all
+        if (!GetComponent<Rigidbody>().SweepTest(transform.forward, out hit, speed * Time.deltaTime + 0.01f))
         {
-            direction *= -1;
+            if (!Front())
+            {
+                transform.Translate(0, 0, translation);
+            }
+            else
+            {
+                transform.Rotate(0, 180, 0);
+            }
+        }
+    }
+
+    public bool Front()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1.0f, layerMask))
+        {
+            Debug.Log("***************ALIEN**********************************Hay algo al frente*******************************************");
+            return true;
+        }
+        else
+        {
+            Debug.Log("No hay algo al frente");
+            return false;
         }
     }
 }
