9122264cedafeba4d7e1d729ecbf7d411910f52e
Jose Hidalgo <jmhc1995@hotmail.com>
2019-02-11 10:21:58 -0600

    Barra de vida, de veneno, minimapa, nueva escena lago

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float currentHealth { get; set; }
+    public float maxHealth { get; set; }
+
+    public Slider health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 100f;
+        currentHealth = maxHealth;
+        health.value = calculateSlideHealth();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I)) {
+            dealDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            gainLife(10);
+        }
+
+    }
+
+    // Reduce the health in "damageValue" points
+    void dealDamage(float damageValue) {
+        currentHealth -= damageValue;
+        // if health gets negative passes to 0
+        if (currentHealth < 0) {
+            currentHealth = 0;
+        }
+        health.value = calculateSlideHealth();
+    }
+
+    // Gain "lifeValue" points of health
+    void gainLife(float lifeValue)
+    {
+        currentHealth += lifeValue;
+
+        // if health gets over 100, it passes to 100
+        if (currentHealth > 100) {
+            currentHealth = 100;
+        }
+        health.value = calculateSlideHealth();
+    }
+
+    // Used to get % between 0 and 1 to use in slider value
+    float calculateSlideHealth() {
+        return currentHealth / maxHealth;
+    }
+}
---
+++ Assets/Scripts/PoisonLevel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoisonLevel : MonoBehaviour
+{
+    public float currentPoison { get; set; }
+    public float maxPoison { get; set; }
+
+    public Slider poison;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxPoison = 3f;
+        currentPoison = 0;
+        poison.value = calculateSlidePoison();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            losePoison(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            getPoison(1);
+        }
+
+    }
+
+    // Reduce the poison poisonPoints
+    void losePoison(float poisonPoints)
+    {
+        currentPoison -= poisonPoints;
+        // if poison level gets negative passes to 0
+        if (currentPoison < 0)
+        {
+            currentPoison = 0;
+        }
+        poison.value = calculateSlidePoison();
+    }
+
+    // Gain poisonPoints of poison level
+    void getPoison(float poisonPoints)
+    {
+        currentPoison += poisonPoints;
+
+        // if poison level gets over 3, it passes to 3
+        if (currentPoison > 3)
+        {
+            currentPoison = 3;
+        }
+        poison.value = calculateSlidePoison();
+    }
+
+    // Used to get % between 0 and 1 to use in slider value
+    float calculateSlidePoison()
+    {
+        return currentPoison / maxPoison;
+    }
+}
