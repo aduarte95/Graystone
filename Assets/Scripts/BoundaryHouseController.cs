13c6f1a1037bf087866266764517b1f31dc3092d
chrisliddell <chrisliddell78958@gmail.com>
2019-02-10 20:17:33 -0600

    no se por que hay tantos scene controllers pero esta cambiando de escena de la nada

---
+++ Assets/Scripts/BoundaryHouseController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoundaryHouseController : MonoBehaviour
+{
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneName = "River Scene";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+		Debug.Log("BOX COLLIDED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        PlayerController player = collider.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            Debug.Log("*******************************************************Loading: " + sceneName + "...");
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
