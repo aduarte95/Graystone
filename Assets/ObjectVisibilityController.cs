618123944a326efb0b3a11da87f27b25198b66a1
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-13 13:37:43 -0600

    Invisible chair

---
+++ Assets/ObjectVisibilityController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectVisibilityController : MonoBehaviour
+{
+    bool isActiveAndEnabled;
+    bool keyIsEnable = false;
+    public GameObject chair;
+
+    public void Update()
+    {
+      if(keyIsEnable && Input.GetKeyDown("q"))
+        {
+            chair.SetActive(!chair.activeSelf);
+        }  
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (!keyIsEnable && collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Colission");
+            keyIsEnable = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (keyIsEnable && other.gameObject.tag == "Player")
+        {
+            Debug.Log("Colission exit");
+            keyIsEnable = false;
+        }
+    }
+}
