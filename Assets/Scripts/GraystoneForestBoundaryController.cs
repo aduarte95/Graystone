d1118cb902ab98959c4e2a3fc068027dbdaa7c59
chrisliddell <chrisliddell78958@gmail.com>
2019-02-13 14:55:22 -0600

    transition between graystone scene and forest scene

---
+++ Assets/Scripts/GraystoneForestBoundaryController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GraystoneForestBoundaryController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+	
+	void Update(){
+		
+	}
+
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision");
+            SceneManager.LoadScene("Forest", LoadSceneMode.Single);
+            //SceneManager.MoveGameObjectToScene(collision.gameObject, SceneManager.GetSceneByName(scenesName));
+        }
+    }
+}
