915f88c0d20d31d00c1667f1962bff0b33d6e47f
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-15 13:28:55 -0600

    Dialogue Controller

---
+++ Assets/Scripts/DialogueController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    public Queue<DialogueTrigger> objectsWithDialogues; //Array of objects that have dialog
+    DialogueTrigger houseDialog;
+    bool isAlienDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        house = objectsWithDialogues.Dequeue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlienDead)
+        {
+            isAlienDead = false; //Just once
+
+
+        }
+    }
+
+    void dequeueDialog()
+    {
+        objectsWithDialogues.Dequeue().TriggerDialogue();
+    }
+
+    //Alien have to change it if he dies.
+    public void setAlienDeadd(bool isAlienDead)
+    {
+        this.isAlienDead = isAlienDead;
+    }
+}
