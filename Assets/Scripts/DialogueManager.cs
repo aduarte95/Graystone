8797f1e25b5b7d21a2289c2632d9653b238aaf8f
Jose Hidalgo <jmhc1995@hotmail.com>
2019-02-15 11:32:16 -0600

    agregado dialogo en Josito

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
 
     private Queue<string> sentences;
 
@@ -15,7 +20,42 @@ public class DialogueManager : MonoBehaviour
     }
 
     public void StartDialogue(Dialogue dialogue) {
-        Debug.Log("Starting conversation with" + dialogue.name);
+
+        animator.SetBool("isOpen", true);
+
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+      
     }
-   
+
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence) {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue() {
+        animator.SetBool("isOpen", false);
+    }
+
+
 }
