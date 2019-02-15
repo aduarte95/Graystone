93c148dba01afae805ae3b9fffc3db18bff3ef0e
Jose Hidalgo <jmhc1995@hotmail.com>
2019-02-15 14:07:11 -0600

    cambios en dialogue trigger

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-   public Dialogue dialogue;
+   public Dialogue[] dialogue;
+    public bool cont;
 
-    public void TriggerDialogue() {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    public void setContinue(bool cont) {
+        this.cont = cont;
+    }
+
+    public void TriggerDialogue(int i) {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue[i]);
     }
 }
