af761e29377160c44cdd2bb1064ced067abcae81
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-15 14:14:18 -0600

    Dialogue Controller

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -6,8 +6,8 @@ public class DialogueController : MonoBehaviour
 {
     public DialogueTrigger[] objectsWithDialogues; //Array of objects that have dialog
     const int HOUSE = 0;
-    const int ALIEN = 1;
     bool isAlienDead = false;
+    bool isPlayerPoisoned = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +18,27 @@ public class DialogueController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isPlayerPoisoned)
+        {
+            isPlayerPoisoned = false; //Just once
+            objectsWithDialogues[HOUSE].TriggerDialogue(1);
+        }
+
         if (isAlienDead)
         {
             isAlienDead = false; //Just once
-            objectsWithDialogues[HOUSE].TriggerDialogue(1);
+            objectsWithDialogues[HOUSE].TriggerDialogue(2);
         }
     }
 
     //Alien have to change it if he dies.
-    public void setAlienDeadd(bool isAlienDead)
+    public void setAlienDead(bool isAlienDead)
     {
         this.isAlienDead = isAlienDead;
+    }
+
+    public void setPlayerPoisoned(bool isPlayerPoisoned)
+    {
+        this.isPlayerPoisoned = isPlayerPoisoned;
     }
 }
