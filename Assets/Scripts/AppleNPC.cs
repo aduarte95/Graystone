865800b648fd1d10e58d099ccb9dd6fdeb73b749
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-22 21:49:44 -0600

    NPC don't talk mad about stolen apples twice

--- Assets/Scripts/AppleNPC.cs
+++ Assets/Scripts/AppleNPC.cs
@@ -12,28 +12,44 @@ public class AppleNPC : NPCController
     private int MaxDist = 10;
     private int MinDist = 2;
     private int isWalkingHash = Animator.StringToHash("IsWalking");
+    private int currentDialogue = 0;
+    private bool StillMad { get; set; } = true; //Avoid npc seacrhes for player if eat another apple
 
     // Update is called once per frame
     void Update()
     {
         if(player.HasEaten) // DEBUG APPLE NPC quitar el false y poner ese cuando player ya tenga el seteo de la variable HasEaten
         {
-            walk();
-            
+           walk(); 
         } else if (CanTalk)
         {
             CanTalk = false;
-            dialogueTrigger.TriggerDialogue(0);
+            if(StillMad)
+            {
+                StillMad = false;
+                dialogueTrigger.TriggerDialogue(currentDialogue);
+            }
+             
         } else
         {
-            if(dialogueTrigger.dialogues[0].Finished)
+            if (dialogueTrigger.dialogues[currentDialogue].Finished)
+            {
+                dialogueTrigger.dialogues[currentDialogue].setDiamondsMission();
+
+                if (currentDialogue < dialogueTrigger.dialogues.Length - 1)
+                {
+                    ++currentDialogue;
+                }
+            }
+
+            /*if(dialogueTrigger.dialogues[0].Finished)
             {
                 nextHouseDiamond.SetActive(true);
                 myDiamonds[0].SetActive(false);
                 myDiamonds[1].SetActive(false);
-            }
+            }*/
         }
-        
+
     }
 
     private void walk()
@@ -42,7 +58,7 @@ public class AppleNPC : NPCController
         transform.LookAt(targetPosition);
 
         // DEBUG APPLE NPC
-        if (Vector3.Distance(transform.position, targetPosition.position) > MinDist)
+        if (Vector3.Distance(transform.position, targetPosition.position) > MinDist && StillMad)
         {
 
             transform.position += transform.forward * MoveSpeed * Time.deltaTime;
